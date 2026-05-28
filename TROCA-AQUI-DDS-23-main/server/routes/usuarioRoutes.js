4d288ef9629ce474e7d520ca51723091fca10359
Matheus Gomes <matheusfavarato13@gmail.com>
2026-05-28 14:31:07 -0300

    gggg

--- TROCA-AQUI-DDS-23-main/server/routes/usuarioRoutes.js
+++ TROCA-AQUI-DDS-23-main/server/routes/usuarioRoutes.js
@@ -5,6 +5,12 @@ const router = express.Router();
 // Importar o controller do usuario
 const usuarioController = require("../controllers/usuarioController.js")
 
+// Importar o multer
+const upload = require("../config/multer.js")
+
+// Importar o middleware de autenticação
+const { verificarAutenticacao, somenteAdmin } = require("../middlewares/authMiddleware.js")
+
 // Declaração das rotas do usuário
 // ROTAS PÚBLICAS
 // Envia os dados de login
@@ -13,17 +19,22 @@ router.post("/login", usuarioController.login)
 // Rota de saida
 router.get("/logout", usuarioController.logout)
 
+// Rota de cadastro de usuário
+// O multer, salva a imagem primeiro, através do upload.single, depois chama o controller
+router.post('/cadastrar', upload.single('foto'), usuarioController.cadastrar )
+
 // ROTAS PRIVADAS
+// Daqui pra baixo, só executa se tiver acesso para tal
+router.use(verificarAutenticacao)
+router.use(somenteAdmin)
 
 // Obtém a lista de usuários
 router.get("/", (req, res) => {
-  res.json({ mensagem: "Peguei a lista de usuários" });
-});
+  res.status(200).render('usuarios/listar')});
 
 //Retornar a página de cadastro
 router.get("/cadastro", (req, res) => {
-  res.json({ mensagem: "Estou na página de cadastro" });
-});
+ res.status(200).render('usuarios/cadastrar')});
 
 
 module.exports = router
